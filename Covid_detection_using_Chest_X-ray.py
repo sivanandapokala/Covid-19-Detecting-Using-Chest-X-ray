2112f78af1f235ca1d820f5f12b9aecfd6f1ec4f
sivanandapokala <50110684+sivanandapokala@users.noreply.github.com>
2021-04-08 17:57:00 +0200

    Add files via upload

---
+++ Covid_detection_using_Chest_X-ray.py
@@ -0,0 +1,66 @@
+from PIL import Image
+import streamlit as st
+import pandas as pd
+from tensorflow import keras
+import numpy as np
+import cv2
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
+from time import sleep
+import streamlit as st
+
+model = keras.models.load_model('C:/Users/sivan/Desktop/ML Projects/Covid-19 Using Chestxray')
+def preprocess(image):
+    print((image))
+    images=[]
+    images.append(cv2.resize(image,(224,224))/255)
+    imag=np.array(images)
+    print(imag.shape)
+    
+    
+    result = model.predict(imag)
+    if np.argmax(result,axis=1)==0:
+        output= 'Covid'
+    elif np.argmax(result,axis=1)==1:
+        output= 'Normal'
+    else:
+        output = 'Viral Pneumonia'
+    return output
+
+
+
+html_temp = """ 
+    <div id=1 style ="background-color:orange;padding:10px"> 
+    <h1 style ="color:blue;text-align:left;">Covid Detection Using Chest X-ray</h1> 
+    </div>
+    
+    <style>
+    body {
+    background-image: url("https://news.usc.edu/files/2018/03/Nanoparticle-cancer-detection-web.jpg");
+    background-size: cover;
+    }
+    </style>
+    """
+
+html1 = """<h2 style ="color:blue;text-align:right;"> - OM AND SIVA</h2> """
+      
+# display the front end aspect
+st.markdown(html_temp, unsafe_allow_html = True) 
+st.markdown(html1, unsafe_allow_html = True)
+st.markdown("""<h2 style ="color:orange;text-align:left;">Please Choose the X-ray to Classify</h2> """, unsafe_allow_html = True)
+      
+    
+
+
+
+img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if img_file_buffer is not None:
+    image = Image.open(img_file_buffer).convert('RGB')
+    image=np.array(image)   
+    pred=preprocess(image)
+    if st.button("Predict"):
+        st.success(pred)
+    
+
+   
